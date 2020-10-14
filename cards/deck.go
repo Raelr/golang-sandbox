e1bb80eb5681887fe6f4c85d383034ccf2aea7c5
Raelr <aryehzinn@hotmail.com>
2020-10-14 15:06:33 +1100

    Added basic deck creation method

---
+++ cards/deck.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Create a new type of deck
+// which is a new slice of strings
+type deck []string
+
+func newDeck() deck {
+
+	newDeck := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			newDeck = append(newDeck, value+" of "+suit)
+		}
+	}
+
+	return newDeck
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
